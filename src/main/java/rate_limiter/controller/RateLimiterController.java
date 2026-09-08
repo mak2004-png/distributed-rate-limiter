f79eb1c089c88e46921fff9c8d9a76630d0dc44b
Affan Khan <maffankhan2004@gmail.com>
2026-09-08 18:22:40 +0500

    Add Redis connectiviy test via Redis Template; Confirm Spring Boot <-> Redis connection works

--- src/main/java/rate_limiter/controller/RateLimiterController.java
+++ src/main/java/rate_limiter/controller/RateLimiterController.java
@@ -1,13 +1,11 @@
 package rate_limiter.controller;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import rate_limiter.bucket.TokenBucket;
 import rate_limiter.service.RateLimiterService;
 
 @RestController
@@ -15,9 +13,12 @@ public class RateLimiterController {
 
     private final RateLimiterService rateLimiterService;
 
-    public RateLimiterController(RateLimiterService rateLimiterService){
+    public RateLimiterController(RateLimiterService rateLimiterService, RedisTemplate<String, String> redisTemplate){
         this.rateLimiterService = rateLimiterService;
+        this.redisTemplate = redisTemplate;
     }
+
+    private final RedisTemplate<String, String> redisTemplate;
 
     @GetMapping("/api/ping")
     public ResponseEntity<Void> userRequest(@RequestParam String clientId){
@@ -28,6 +29,12 @@ public class RateLimiterController {
         else {
             return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
         }
+    }
+
+    @GetMapping("/api/redis-test")
+    public String redisTest(){
+        redisTemplate.opsForValue().set("test key", "hello redis");
+        return redisTemplate.opsForValue().get("test key");
     }
 
 }
